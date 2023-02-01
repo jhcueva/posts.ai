cc82357f0aadd8bd08ea9f16463951da3a37dc87
jhcueva <humbertocueva97@gmail.com>
2023-02-01 15:24:39 -0500

    [ADD] LinkedIn card

--- src/components/SocialNetworkCard.tsx
+++ src/components/SocialNetworkCard.tsx
@@ -2,6 +2,7 @@ import { useStore } from "@nanostores/preact";
 import { socialNetwork } from "./store";
 import TwitterCard from "./Twitter";
 import FacebookCard from "./Facebook";
+import LinkedInCard from "./LinkedInCard";
 
 function SocialNetworkCard() {
   const $socialNetwork = useStore(socialNetwork)
@@ -9,6 +10,7 @@ function SocialNetworkCard() {
     <>
       {$socialNetwork === "twitter" ? <TwitterCard /> : null}
       {$socialNetwork === "facebook" ? <FacebookCard /> : null}
+      {$socialNetwork === "linkedin" ? <LinkedInCard /> : null}
     </>
   )
 }
